3fff49469d85b7d5b6e8f4fa15e9adb779a8c5fe
Jeremy <JeremyPlott@gmail.com>
2019-10-18 14:21:41 -0400

    sdf

--- HRElectronicsShop/Program.cs
+++ HRElectronicsShop/Program.cs
@@ -7,8 +7,21 @@ namespace HRElectronicsShop {
     class Program {
         static void Main(string[] args) {
 
+            int[] keyboards = { 12, 12 };
+            int[] drives = { 5, 2, 8 };
 
+            int b = 10;
 
+            var spent = -1;      
+
+            foreach(int n in keyboards) {
+                foreach(int m in drives) {                    
+                    if(n + m <= b && n + m > spent) {
+                        spent = n + m;
+                    }
+                }
+            }
+            return spent;
         }
     }
 }
