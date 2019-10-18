335a21047be81549f1103548122d9b391e95ffda
Jeremy <JeremyPlott@gmail.com>
2019-10-18 12:31:28 -0400

    Add project files.

---
+++ HRElectronicsShop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HRElectronicsShop {
+    class Program {
+        static void Main(string[] args) {
+
+
+
+        }
+    }
+}
